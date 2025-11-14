83f90ed561d3432d918488f9765c76a9d2a7b122
Prakash <mathaprakash8712@gmail.com>
2025-11-14 14:22:26 +0530

    Add diet images;rename hero.jpg to 11.jpg; updated style.css

--- js/main.js
+++ js/main.js
@@ -5,30 +5,79 @@ window.addEventListener('scroll', () => {
 });
 
 // Diet Plan Slider
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slide");
-const next = document.querySelector(".next");
-const prev = document.querySelector(".prev");
+//let currentSlide = 0;
+//const slides = document.querySelectorAll(".slide");
+//const next = document.querySelector(".next");
+//const prev = document.querySelector(".prev");
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove("active");
-    if (i === index) slide.classList.add("active");
-  });
-}
+//function showSlide(index) {
+ // slides.forEach((slide, i) => {
+//    slide.classList.remove("active");
+  //  if (i === index) slide.classList.add("active");
+  //});
+//}
 
-next.addEventListener("click", () => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-});
+//next.addEventListener("click", () => {
+  //currentSlide = (currentSlide + 1) % slides.length;
+ // showSlide(currentSlide);
+//});
 
-prev.addEventListener("click", () => {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-});
+//prev.addEventListener("click", () => {
+ // currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+ // showSlide(currentSlide);
+//});
 
 // Auto-slide every 5 seconds
+//setInterval(() => {
+ // currentSlide = (currentSlide + 1) % slides.length;
+  //showSlide(currentSlide);
+//}, 5000);
+// WEIGHT LOSS SLIDER
+let lossIndex = 0;
+const lossSlides = document.querySelectorAll(".loss-slide");
+
+function showLossSlide(i) {
+  lossSlides.forEach(s => s.classList.remove("active"));
+  lossSlides[i].classList.add("active");
+}
+
+document.querySelector(".next-loss").onclick = function () {
+  lossIndex = (lossIndex + 1) % lossSlides.length;
+  showLossSlide(lossIndex);
+};
+
+document.querySelector(".prev-loss").onclick = function () {
+  lossIndex = (lossIndex - 1 + lossSlides.length) % lossSlides.length;
+  showLossSlide(lossIndex);
+};
+
 setInterval(() => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}, 5000);
+  lossIndex = (lossIndex + 1) % lossSlides.length;
+  showLossSlide(lossIndex);
+}, 4000);
+
+
+// WEIGHT GAIN SLIDER
+let gainIndex = 0;
+const gainSlides = document.querySelectorAll(".gain-slide");
+
+function showGainSlide(i) {
+  gainSlides.forEach(s => s.classList.remove("active"));
+  gainSlides[i].classList.add("active");
+}
+
+document.querySelector(".next-gain").onclick = function () {
+  gainIndex = (gainIndex + 1) % gainSlides.length;
+  showGainSlide(gainIndex);
+};
+
+document.querySelector(".prev-gain").onclick = function () {
+  gainIndex = (gainIndex - 1 + gainSlides.length) % gainSlides.length;
+  showGainSlide(gainIndex);
+};
+
+setInterval(() => {
+  gainIndex = (gainIndex + 1) % gainSlides.length;
+  showGainSlide(gainIndex);
+}, 4000);
+
